5b58d8e0629a4fc6583927916cd5275789eea96e
Daryl Turner <d.turner@arkadin.com>
2019-05-10 17:20:59 +0100

    aNIWv from ergodox ez configurator

---
+++ ergodox_ez_daryl-v00_source/config.h
@@ -0,0 +1,16 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 30
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 90
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 2
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 60
+
